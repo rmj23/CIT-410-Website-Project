63e43375d402dae22c6b0f4c5c638ed89dfe8af9
Ruben Morales Jr <rm6839@uncw.edu>
2018-11-07 10:20:05 -0500

    Update form.php

--- form.php
+++ form.php
@@ -1,4 +1,22 @@
 <?
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:cit410projectserver.database.windows.net,1433; Database = cit410-database", "username", "Password");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "rubenadmin@cit410projectserver", "pwd" => "Pssword", "Database" => "cit410-database", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:cit410projectserver.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+
+
+
+// write code below
 
 
 ?>
